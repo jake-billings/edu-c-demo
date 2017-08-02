095c8072e2573d64f8e273a8fd627a00f6f9f8ec
jakebillings <jake@jakebillings.com>
2017-08-02 15:34:19 -0600

    Add search inventory by model

--- functions.cpp
+++ functions.cpp
@@ -38,10 +38,10 @@ bool userInterfaceLoop(std::vector<Vehicle>& inventory) {
             deleteVehicle(inventory);
             break;
         case 5:
-//            sortInventoryByVIN(inventory);
+            sortInventoryByVIN(inventory);
             break;
         case 6:
-//            sortInventoryByModel(inventory);
+            searchInventoryByModel(inventory);
             break;
         case 7:
 //            readInventoryFromFile(inventory);
@@ -93,6 +93,46 @@ void deleteVehicle(std::vector<Vehicle>& inventory) {
     inventory.erase(inventory.begin() + i);
     std::cout << std::endl << "Deleted vehicle from inventory." << std::endl;
 }
+
+void sortInventoryByVIN(std::vector<Vehicle>& inventory) {
+    //Using bubble sort, arrange Vehicle from tallest to shortest in the inventory vector.
+    for (unsigned long i = 0; i<inventory.size(); i++) {
+        for (unsigned long j = 0; j<inventory.size()-1; j++) {
+            Vehicle v = inventory[j];
+            if (v.getVIN() > inventory[j+1].getVIN()) {
+                inventory[j] = inventory[j+1];
+                inventory[j+1] = v;
+            }
+        }
+    }
+    std::cout << std::endl << "Sorted vehicles." << std::endl;
+}
+
+void searchInventoryByModel(std::vector<Vehicle>& inventory) {
+    std::cout<<"Search for Model: ";
+    std::string searchText;
+    std::cin >> searchText;
+
+    std::vector<Vehicle> results;
+    for (unsigned long i = 0; i<inventory.size(); i++) {
+        if (inventory[i].getModel()==searchText) {
+            results.push_back(inventory[i]);
+        }
+    }
+
+    if (results.size()>0) {
+        std::cout<<"Found the following vehicles: "<<std::endl<<std::endl;
+    } else {
+        std::cout<<"No vehicles were found matching that model."<<std::endl;
+    }
+
+    for (unsigned long i = 0; i<results.size(); i++) {
+        std::cout<<"Result #: "<<i<<std::endl;
+        std::cout<<results[i]<<std::endl;
+    }
+
+}
+
 
 void unknownOption() {
     std::cout << "Unknown Option" << std::endl;
